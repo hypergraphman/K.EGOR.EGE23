f122a3b5986af636d757e80bb6e7aad18a4c3ef1
Руслан Аитов <r_aitov@mail.ru>
2022-12-19 17:56:16 +0300

    zad 26

--- task26/72.py
+++ task26/72.py
@@ -1,19 +1,32 @@
-f = open('test.txt')
-n, m = map(int, f.readline().split())
+f = open('26-72.txt')
+n, m, k = map(int, f.readline().split())
 s = dict()
-for _ in range(n):
+for _ in range(k):
     y, x = map(int, f.readline().split())
     if y in s:
         s[y].append(x)
     else:
-        s[y] = [x]
+        s[y] = [0, x, n + 1]
 
-c = 0
-for y in range(n):
-    if m > sum(s[y]):
-        c += 1
+for r in s:
+    s[r].sort()
+
+print(s)
+
+count = 0
+mx_r = 0
+mx = 0
+for r in range(1, m + 1):
+    count_r = 0
+    if r in s:
+        for i in range(1, len(s[r])):
+            empty = s[r][i] - s[r][i - 1] - 1
+            if empty - 3 > 0:
+                count_r += empty - 3
     else:
-
-print(c)
-
-
+        count_r = n - 3
+    count += count_r
+    if count_r > mx:
+        mx = count_r
+        mx_r = r
+print(count, mx_r)
